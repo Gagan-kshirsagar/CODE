dbebc8fb52fdda64efc9bb46e61c66b561ce2538
name <email>
2024-02-11 18:00:37 +0530

    Added Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Router, Route } from 'react-router-dom'
+
+import { Home } from './pages/index'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
